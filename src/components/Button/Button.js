a1658d8125db7445bd3c66b3075c097b22c072fe
Alen Rajher <arajher@mono.software>
2022-10-22 20:01:30 +0200

    adding collapse

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 import btn from "./Button.module.css";
 
-const Button = ({ type, size, value }) => {
+const Button = ({ type, size, value, action }) => {
     return (
-        <button className={`${btn.common} ${btn[type]} ${btn[size]}`}>
+        <button className={`${btn.common} ${btn[type]} ${btn[size ? size : "base"]}`} onClick={action}>
             {value}
         </button>
     );
--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import menu from "./MainMenu.module.css";
 import Link from "next/Link";
 import menuItems from "./menuStore";
 import { useRouter } from "next/router";
+import { Button } from "components";
 
 const MainMenu = () => {
+    const [isCollapsed, setisCollapsed] = useState(false);
+
+    const handleMenu = () => {
+        setisCollapsed(!isCollapsed);
+    };
+    console.log(isCollapsed);
     return (
-        <div className={menu.main}>
+        <div className={`${menu.main} ${isCollapsed ? menu.isCollapsed : ""}`}>
             {menuItems.map((item) => (
                 <React.Fragment key={item.id}>
-                    <MenuItem title={item.title} path={item.path} />
+                    <MenuItem
+                        title={item.title}
+                        path={item.path}
+                        isCollapsed={isCollapsed}
+                    />
                 </React.Fragment>
             ))}
+
+            <div className={menu.link}>
+                <Button value={isCollapsed ? "C" : "Collapsed"} type={"primary"} action={handleMenu} />
+            </div>
         </div>
     );
 };
 
 export default MainMenu;
 
-const MenuItem = ({ title, path }) => {
+const MenuItem = ({ title, path, isCollapsed }) => {
     const router = useRouter();
+
+    const icon = <i>{title[0]}</i>;
 
     return (
         <div
@@ -28,7 +45,7 @@ const MenuItem = ({ title, path }) => {
             }`}
         >
             <Link href={path}>
-                <a className={menu.link}>{title}</a>
+                <a className={menu.link}>{isCollapsed ? icon : title}</a>
             </Link>
         </div>
     );
